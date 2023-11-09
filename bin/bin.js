68aa1ceac9f4fbe572ddd5fed5325f4264cae70c
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2023-11-10 00:56:51 +0100

    fix: spawn relative to file

--- bin/bin.js
+++ bin/bin.js
@@ -1,5 +1,9 @@
 import { fork } from 'node:child_process'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // make sure new process runs with inspect
 process.execArgv.push('--inspect')
-fork('index.js')
+fork(resolve(__dirname, '..', 'index.js'))
