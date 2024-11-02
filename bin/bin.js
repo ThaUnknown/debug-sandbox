018c84a278d9baad76e6f867f6b08d6de936e09b
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2024-11-02 21:10:08 +0100

    fix: new node import handling ,doesnt support sourcemaps :/

--- bin/bin.js
+++ bin/bin.js
@@ -3,6 +3,5 @@ import { dirname, resolve } from 'path'
 import { fileURLToPath } from 'url'
 
 // make sure new process runs with inspect and network imports
-process.execArgv.push('--inspect')
-process.execArgv.push('--experimental-network-imports')
-fork(resolve(dirname(fileURLToPath(import.meta.url)), '..', 'index.js'), ['--experimental-network-imports'])
+process.execArgv.push('--inspect', '--enable-source-maps')
+fork(resolve(dirname(fileURLToPath(import.meta.url)), '..', 'index.js'), ['--enable-source-maps'])
--- importHooks.mjs
+++ importHooks.mjs
@@ -1,4 +1,13 @@
-export async function resolve (specifier, context, nextResolve) {
-  if (specifier?.startsWith?.('node:')) return nextResolve(specifier, { ...context, parentURL: import.meta.url })
-  return nextResolve(specifier)
+/** @type {import('node:module').LoadHook} */
+export async function load (url, context, nextLoad) {
+  if (url.startsWith('https://')) {
+    const res = await fetch(url)
+    return {
+      format: 'module',
+      shortCircuit: true,
+      source: await res.text()
+    }
+  }
+
+  return nextLoad(url)
 }
